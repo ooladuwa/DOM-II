a82fb3b507c2616279ac701f92420e88aaeaab78
Onaje Oladuwa <onaje.oladuwa@gmail.com>
2020-12-01 19:46:05 -0500

    finished 6 event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,59 @@
 // Your code goes here
+// * `mouseenter / mouseleave`
+const head = document.querySelector('.logo-heading')
+    head.addEventListener('mouseenter', (event) => {
+        head.style.color = 'dodgerBlue';
+    });
+    head.addEventListener('mouseleave', (event) => {
+        head.style.color = 'black'
+    });
+
+
+// * 'click'
+const links = document.querySelectorAll('nav a')
+    links.addEventListener('click', () => {
+        links.style.color = 'purple'
+        // links.classList.add('sky');
+        // links.classList.remove('sky');
+        // setInterval(() => links.classList.toggle('sky'), 1000)
+    }); 
+
+
+// * 'mouseenter  
+const busImg = document.querySelector('header img')
+    busImg.addEventListener('mouseenter', (event) => {
+        busImg.style.border = '2px dotted blue'
+    });
+    
+// * 'click
+let signUpButton = document.querySelectorAll('.btn');
+    signUpButton.addEventListener('dblclick', (event) => {     
+ // signUpButton.style.backgroundColor = 'blue'
+        alert("You're ready to ride the bus!")
+    })
+    
+//   * `keydown`
+
+
+//   * `wheel`
+
+
+//   * `load`
+
+
+//   * `focus`
+
+
+//   * `resize`
+
+
+//   * `scroll`
+
+
+//   * `select`
+
+
+//   * `dblclick`
+
+
+//   * `drag / drop`
